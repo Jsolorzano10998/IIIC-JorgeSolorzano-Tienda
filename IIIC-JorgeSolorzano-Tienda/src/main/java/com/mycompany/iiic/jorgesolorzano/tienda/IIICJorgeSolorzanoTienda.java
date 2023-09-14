ecec66dbd793c748ba52e217eb07111345fcac7a
Jsolorzano10998 <133933230+Jsolorzano10998@users.noreply.github.com>
2023-09-13 20:47:04 -0600

    Semana1-Prueba

---
+++ IIIC-JorgeSolorzano-Tienda/src/main/java/com/mycompany/iiic/jorgesolorzano/tienda/IIICJorgeSolorzanoTienda.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.iiic.jorgesolorzano.tienda;
+
+/**
+ *
+ * @author LABORATORIO 04
+ */
+public class IIICJorgeSolorzanoTienda {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

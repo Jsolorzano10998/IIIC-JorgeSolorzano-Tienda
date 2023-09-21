86911b072a93c72400f5a3cdb58f6df635a218cf
Jsolorzano10998 <133933230+Jsolorzano10998@users.noreply.github.com>
2023-09-20 20:46:16 -0600

    Semana2

---
+++ IIIC-JorgeSolorzano-Tienda/src/main/java/Tienda_JorgeSolorzano/demo/TiendaJorgeSolorzanoApplication.java
@@ -0,0 +1,13 @@
+package Tienda_JorgeSolorzano.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaJorgeSolorzanoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaJorgeSolorzanoApplication.class, args);
+	}
+
+}
---
+++ IIIC-JorgeSolorzano-Tienda/src/test/java/Tienda_JorgeSolorzano/demo/TiendaJorgeSolorzanoApplicationTests.java
@@ -0,0 +1,13 @@
+package Tienda_JorgeSolorzano.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaJorgeSolorzanoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
